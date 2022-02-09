4d3079b02d8c0419c9c3d8c46b7bad6567b6417b
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2022-02-09 13:57:24 +0200

    Add web package

--- packages/api/src/app.ts
+++ packages/api/src/app.ts
@@ -1,7 +1,9 @@
+import { add } from "@walnut/common";
 import cors from "cors";
 import express, { Request, Response } from "express";
 import helmet from "helmet";
 import morgan from "morgan";
+import logger from "./config/logger";
 
 const app = express();
 
@@ -9,6 +11,8 @@ app.use(express.json());
 app.use(morgan("tiny"));
 app.use(helmet());
 app.use(cors());
+
+logger.info(add(1, 1));
 
 app.get("/", (_req: Request, res: Response) => {
   res.json({ message: "Server running" });
